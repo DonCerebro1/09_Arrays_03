14c036124946dcb9a2d5e20a1b79dc017710e978
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-11-25 12:50:20 +0100

    Done

--- arr_01.js
+++ arr_01.js
@@ -9,11 +9,58 @@ Verwenden Sie dafür die untenstehenden Arrays
 */
 
 const controls = ["<", "</", ">"];
-const tags = ["html","head","head","body","h1","h1","p","p",","body","html"];
+const tags = ["html","head","head","body","h1","h1","p","p","body","html"];
 let stack = [];
 
+output(getHtml());
+function getHtml()
+ { 
+    let htmlStr = "";
 
+    for (let i = 0; i < tags.length; i++) 
+    {
+        if (isOpenTag(tags[i])) 
+        {
+            htmlStr += getTags(tags[i],"open");
+        } else
+         {
+            htmlStr += getTags(tags[i],"close");
+         }
+    }
+    return htmlStr;
+}
 
+// output(getTags("html", "open"));
+// output(getTags("html", "close"));
+function getTags(tag, op)
+{
+    switch(op)
+    {
+        case "open":
+            return controls[0] + tag + controls[2];
+        case "close":
+            return controls[1] + tag + controls[2];   
+        default:
+            return "#!";    
+    } 
+}
+
+function isOpenTag(tag)
+{
+    // tag == mitgebrachter tag gleich dem oben auf dem Stapel
+    const cond =  tag != stack[stack.length - 1];  
+
+    if (cond)
+    {
+        stack.push(tag);
+        return true;
+        
+    } else
+      {
+        stack.pop();  
+        return false;
+      }  
+}
 
 // Modul: Ausgabe | Test
 //output("hi");
